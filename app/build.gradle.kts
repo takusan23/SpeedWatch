38a5185a7551e03cf312f19be28460795639ad97
takusan23 <takumu1200@gmail.com>
2023-09-24 14:26:27 +0900

    AGP / Gradle / targetSdk の更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "io.github.takusan23.speedwatch"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "io.github.takusan23.speedwatch"
         minSdk = 30
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 2
         versionName = "1.0.1"
         vectorDrawables {
@@ -35,7 +35,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.1.1"
+        kotlinCompilerExtensionVersion = "1.4.7"
     }
     packagingOptions {
         resources {
@@ -45,25 +45,24 @@ android {
 }
 
 dependencies {
-    val compose_version = "1.2.0"
-    val wear_compose_version = "1.0.0"
+    val compose_version = "1.5.1"
+    val wear_compose_version = "1.2.0"
 
-    implementation("com.google.android.gms:play-services-location:21.0.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.wear:wear-remote-interactions:1.0.0")
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("com.google.android.gms:play-services-wearable:18.0.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.google.android.gms:play-services-wearable:18.1.0")
     implementation("androidx.percentlayout:percentlayout:1.0.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
+
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.ui:ui:$compose_version")
     implementation("androidx.wear.compose:compose-material:$wear_compose_version")
     implementation("androidx.wear.compose:compose-foundation:$wear_compose_version")
     implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
     implementation("androidx.wear.compose:compose-navigation:$wear_compose_version")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose_version")
     debugImplementation("androidx.compose.ui:ui-tooling:$compose_version")
     debugImplementation("androidx.compose.ui:ui-test-manifest:$compose_version")
